6476968733b323f09f90b2fe4b1aa75d3c3bb346
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-04-01 21:39:11 +0700

    fix: structureFile

---
+++ src/gui/KhachSanAnNhien_GUI.java
@@ -0,0 +1,22 @@
+package gui;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class KhachSanAnNhien_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
