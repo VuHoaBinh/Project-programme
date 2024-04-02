ce2869cd107dee22ccd073f012ee99b7484e9dad
Quang Nhân <tranquangnhan2003@gmail.com>
2024-04-02 08:28:37 +0700

    Gần xong entity, còn các phương thức tính toán

---
+++ src/entity/ChiTietHoaDon.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ChiTietHoaDon {
+    private HoaDon hoaDon;
+    private DoAnUong doAnUong;
+    private int soLuong;
+    private Phong phong;
+    private LocalDate ngayNhanPhong;
+    private LocalDate ngayTraPhong;
+    private int soLuongNguoiO;
+    private int soLuongDoUongTraVe;
+    private double tongTienThuePhong;
+    private double tongTienDichVu;
+    private double tongThanhTien;
+    private double phuPhi;
+
+    public ChiTietHoaDon() {
+    }
+
+    public ChiTietHoaDon(HoaDon hoaDon, DoAnUong doAnUong, int soLuong, Phong phong, LocalDate ngayNhanPhong, LocalDate ngayTraPhong, int soLuongNguoiO, int soLuongDoUongTraVe, double tongTienThuePhong, double tongTienDichVu, double tongThanhTien, double phuPhi) {
+        this.hoaDon = hoaDon;
+        this.doAnUong = doAnUong;
+        this.soLuong = soLuong;
+        this.phong = phong;
+        this.ngayNhanPhong = ngayNhanPhong;
+        this.ngayTraPhong = ngayTraPhong;
+        this.soLuongNguoiO = soLuongNguoiO;
+        this.soLuongDoUongTraVe = soLuongDoUongTraVe;
+        this.tongTienThuePhong = tongTienThuePhong;
+        this.tongTienDichVu = tongTienDichVu;
+        this.tongThanhTien = tongThanhTien;
+        this.phuPhi = phuPhi;
+    }
+
+    public HoaDon getHoaDon() {
+        return hoaDon;
+    }
+
+    public DoAnUong getDoAnUong() {
+        return doAnUong;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public Phong getPhong() {
+        return phong;
+    }
+
+    public LocalDate getNgayNhanPhong() {
+        return ngayNhanPhong;
+    }
+
+    public LocalDate getNgayTraPhong() {
+        return ngayTraPhong;
+    }
+
+    public int getSoLuongNguoiO() {
+        return soLuongNguoiO;
+    }
+
+    public int getSoLuongDoUongTraVe() {
+        return soLuongDoUongTraVe;
+    }
+
+    public double getTongTienThuePhong() {
+        return tongTienThuePhong;
+    }
+
+    public double getTongTienDichVu() {
+        return tongTienDichVu;
+    }
+
+    public double getTongThanhTien() {
+        return tongThanhTien;
+    }
+
+    public double getPhuPhi() {
+        return phuPhi;
+    }
+
+    public void setHoaDon(HoaDon hoaDon) {
+        this.hoaDon = hoaDon;
+    }
+
+    public void setDoAnUong(DoAnUong doAnUong) {
+        this.doAnUong = doAnUong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void setPhong(Phong phong) {
+        this.phong = phong;
+    }
+
+    public void setNgayNhanPhong(LocalDate ngayNhanPhong) {
+        this.ngayNhanPhong = ngayNhanPhong;
+    }
+
+    public void setNgayTraPhong(LocalDate ngayTraPhong) {
+        this.ngayTraPhong = ngayTraPhong;
+    }
+
+    public void setSoLuongNguoiO(int soLuongNguoiO) {
+        this.soLuongNguoiO = soLuongNguoiO;
+    }
+    public double tinhTongTienDichVu(){
+        return soLuong * doAnUong.getGiaNhap();
+    }
+//    public double tinhTienThuePhong(){
+//        
+//    }
+//    public double tinhTongThanhTien(){
+//        return 
+//    }
+//    
+    //tinhTongPhuPhi (tl4 thiếu cái này)
+    @Override
+    public String toString() {
+        return "ChiTietHoaDon{" + "hoaDon=" + hoaDon + ", doAnUong=" + doAnUong + ", soLuong=" + soLuong + ", phong=" + phong + ", ngayNhanPhong=" + ngayNhanPhong + ", ngayTraPhong=" + ngayTraPhong + ", soLuongNguoiO=" + soLuongNguoiO + ", soLuongDoUongTraVe=" + soLuongDoUongTraVe + ", tongTienThuePhong=" + tongTienThuePhong + ", tongTienDichVu=" + tongTienDichVu + ", tongThanhTien=" + tongThanhTien + ", phuPhi=" + phuPhi + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.hoaDon);
+        hash = 53 * hash + Objects.hashCode(this.doAnUong);
+        hash = 53 * hash + Objects.hashCode(this.phong);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        final ChiTietHoaDon other = (ChiTietHoaDon) obj;
+        if (!Objects.equals(this.hoaDon, other.hoaDon)) {
+            return false;
+        }
+        if (!Objects.equals(this.doAnUong, other.doAnUong)) {
+            return false;
+        }
+        return Objects.equals(this.phong, other.phong);
+    }
+    
+    
+    
+}
---
+++ src/entity/PhieuDatPhong.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PhieuDatPhong {
+    private String maPhieuDatPhong;
+    private KhachHang khachHang;
+    private NhanVien nhanvien;
+    private int soLuongNguoi;
+    private LocalDate ngayDatPhong;
+    private LocalDate ngayNhanPhong;
+    private LocalDate ngayTraPhong;
+    private double tienPhong;
+
+    public PhieuDatPhong() {
+    }
+
+    public PhieuDatPhong(String maPhieuDatPhong, KhachHang khachHang, NhanVien nhanvien, int soLuongNguoi, LocalDate ngayDatPhong, LocalDate ngayNhanPhong, LocalDate ngayTraPhong, double tienPhong) {
+        this.maPhieuDatPhong = maPhieuDatPhong;
+        this.khachHang = khachHang;
+        this.nhanvien = nhanvien;
+        this.soLuongNguoi = soLuongNguoi;
+        this.ngayDatPhong = ngayDatPhong;
+        this.ngayNhanPhong = ngayNhanPhong;
+        this.ngayTraPhong = ngayTraPhong;
+        this.tienPhong = tienPhong;
+    }
+
+    public String getMaPhieuDatPhong() {
+        return maPhieuDatPhong;
+    }
+
+    public KhachHang getKhachHang() {
+        return khachHang;
+    }
+
+    public NhanVien getNhanvien() {
+        return nhanvien;
+    }
+
+    public int getSoLuongNguoi() {
+        return soLuongNguoi;
+    }
+
+    public LocalDate getNgayDatPhong() {
+        return ngayDatPhong;
+    }
+
+    public LocalDate getNgayNhanPhong() {
+        return ngayNhanPhong;
+    }
+
+    public LocalDate getNgayTraPhong() {
+        return ngayTraPhong;
+    }
+
+    public double getTienPhong() {
+        return tienPhong;
+    }
+
+    public void setMaPhieuDatPhong(String maPhieuDatPhong) {
+        this.maPhieuDatPhong = maPhieuDatPhong;
+    }
+
+    public void setKhachHang(KhachHang khachHang) {
+        this.khachHang = khachHang;
+    }
+
+    public void setNhanvien(NhanVien nhanvien) {
+        this.nhanvien = nhanvien;
+    }
+
+    public void setSoLuongNguoi(int soLuongNguoi) {
+        this.soLuongNguoi = soLuongNguoi;
+    }
+
+    public void setNgayDatPhong(LocalDate ngayDatPhong) {
+        this.ngayDatPhong = ngayDatPhong;
+    }
+
+    public void setNgayNhanPhong(LocalDate ngayNhanPhong) {
+        this.ngayNhanPhong = ngayNhanPhong;
+    }
+
+    public void setNgayTraPhong(LocalDate ngayTraPhong) {
+        this.ngayTraPhong = ngayTraPhong;
+    }
+
+    @Override
+    public String toString() {
+        return "PhieuDatPhong{" + "maPhieuDatPhong=" + maPhieuDatPhong + ", khachHang=" + khachHang + ", nhanvien=" + nhanvien + ", soLuongNguoi=" + soLuongNguoi + ", ngayDatPhong=" + ngayDatPhong + ", ngayNhanPhong=" + ngayNhanPhong + ", ngayTraPhong=" + ngayTraPhong + ", tienPhong=" + tienPhong + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        final PhieuDatPhong other = (PhieuDatPhong) obj;
+        return Objects.equals(this.maPhieuDatPhong, other.maPhieuDatPhong);
+    }
+    //tinhTienPhong
+    
+}
